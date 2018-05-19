7bc02e63e441eda594b3bb7853b0c78246eed2d3
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-05-20 02:10:26 +0300

    Fix.

---
+++ Lesson3/Problems.fs
@@ -0,0 +1,55 @@
+﻿let findEvenMap list =
+    list |> List.map (fun i -> ((abs i) + 1) % 2) |> List.sum 
+
+let findEvenFilter list =
+    list |> List.filter (fun i -> (abs i) % 2 = 0) |> List.length
+
+let findEvenFold = 
+    List.fold (fun acc i -> acc + ((abs i) + 1) % 2) 0
+
+type Tree<'a> = 
+    | Node of 'a * Tree<'a> * Tree<'a> 
+    | Empty
+
+let rec mapTree tree func =
+    match tree with
+    | Empty -> tree
+    | Node(v, left, right) -> Node(func v, mapTree left func, mapTree right func)
+    
+type ArifmeticalProp = 
+    | Num of int 
+    | Sum of ArifmeticalProp * ArifmeticalProp 
+    | Subtraction of ArifmeticalProp * ArifmeticalProp 
+    | Multiplication of ArifmeticalProp * ArifmeticalProp
+    | Division of ArifmeticalProp * ArifmeticalProp
+    | Negative of ArifmeticalProp
+
+let rec eval (p: ArifmeticalProp) = 
+    match p with 
+    | Num(a) -> a 
+    | Sum(a, b) -> eval a + eval b 
+    | Subtraction(a, b) -> eval a - eval b 
+    | Multiplication(a, b) -> eval a * eval b
+    | Division(a, b) -> eval a / eval b
+    | Negative(a) -> -(eval a)
+
+
+let seqOfPrime = 
+    let isPrime (primeNums : seq<int>) num =
+        primeNums |> Seq.takeWhile (fun x -> x * x <= num) |> Seq.map (fun x -> not (num % x = 0)) |> Seq.fold (fun acc x -> acc && x) true
+
+    let rec seqOfPrimeRec (primeNums : seq<int>) (nums : seq<int>) =    
+        seq {
+            if (isPrime primeNums (Seq.head nums)) 
+            then 
+                yield (Seq.head nums)
+                yield! seqOfPrimeRec (Seq.append primeNums [Seq.head nums]) (Seq.tail nums) 
+            else 
+                yield! seqOfPrimeRec primeNums (Seq.tail nums)
+        }
+    seqOfPrimeRec Seq.empty (Seq.initInfinite (fun x -> x + 2))
+
+[<EntryPoint>]
+let main argv =
+    0
+    
---
+++ Lesson3/TestProblems/Test.fs
@@ -0,0 +1,59 @@
+﻿open NUnit.Framework 
+open FsUnit
+open Problems
+open System
+
+[<Test>]
+let ``test tree`` () =
+    mapTree (Node(10, (Node(7, Node(5, Empty, Empty), Node(8, Empty, Empty))), (Node(21, Node(20, Empty, Empty), Node(25, Empty, Empty))))) (fun i -> i + 5)
+    |> should equal (Node(15, (Node(12, Node(10, Empty, Empty), Node(13, Empty, Empty))), (Node(26, Node(25, Empty, Empty), Node(30, Empty, Empty))))) 
+
+[<Test>]
+let ``test of arifmetic 1`` () =
+    eval(Sum(Subtraction(Num(5), Num(4)), Multiplication(Num(1), Negative(Num(1)))))
+    |> should equal 0
+
+[<Test>]
+let ``test of arifmetic 2`` () =
+    (fun() -> eval(Division(Num(666), Num(0))) |> ignore) |> should throw typeof<DivideByZeroException> 
+
+[<Test>]
+let ``test of arifmetic 3`` () =
+    eval(Multiplication(Subtraction(Num(3), Num(5)), Negative(Num(2)))) |> should equal 4
+
+[<Test>]
+let ``test of prime numbers`` () =
+    (Seq.toList (Seq.take 20 seqOfPrime)) |> should equal [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71]
+
+let findEven n =
+    match n with
+    | 0 -> findEvenFilter
+    | 1 -> findEvenFold
+    | 2 -> findEvenMap
+
+[<Test>]
+let ``test of find even 1`` () =
+    for i in 0..2 do
+        [1; 2; -3; 4; -5; 6; 7] |> findEven i |> should equal 3
+
+[<Test>]
+let ``test of find even 2`` () =
+    for i in 0..2 do
+        [1] |> findEven i |> should equal 0
+
+[<Test>]
+let ``test of find even 3`` () =
+    for i in 0..2 do
+        [] |> findEven i |> should equal 0
+
+[<EntryPoint>]
+let main argv =
+    ``test tree`` ()
+    ``test of arifmetic 1`` ()
+    ``test of arifmetic 2`` ()
+    ``test of arifmetic 3`` ()
+    ``test of prime numbers`` ()
+    ``test of find even 1`` ()
+    ``test of find even 2``()
+    ``test of find even 3`` ()
+    1
